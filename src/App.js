c42f27ee52fc27ea7f0e11988b1b2bf80965fbed
Taylor Burke <taylorburke@Taylors-MBP.lan>
2023-10-25 18:35:43 -0600

    basic search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Checkbox, FormControlLabel, FormGroup } from '@material-ui/core';
+import { Checkbox, FormControlLabel, FormGroup, TextField } from '@material-ui/core';
 import {items} from './data/glossaryItems'
 import { sortAlphabetic } from './utils'
 
 function App() {
   const [glossaryItems, setGlossaryItems] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
   const categories = [...new Set(items.map(item => item.category))];
 
@@ -18,14 +19,20 @@ function App() {
       }
   }
 
+  // handle search text to filter items
+  useEffect(() => {
+    setGlossaryItems(sortAlphabetic(items.filter(item => item.glossaryItem.toLowerCase().includes(searchText.toLowerCase()))));
+  }, [searchText]);
+  
   return (
     <div className="App">
       <header className="App-header">
       {/* <a href="../public/logo192.png" download="logo">Download</a> */}
       <h1>Climbing Terms</h1>
-        <FormGroup className="categoriesChecks">{categories.map(category => 
-        <FormControlLabel className='checkBox' key={category} label={category} control={<Checkbox value={category} onChange={(e)=>handleToggle(e)} />} />)}
+        <FormGroup>{categories.map(category => 
+        <FormControlLabel  key={category} label={category} control={<Checkbox value={category} onChange={(e)=>handleToggle(e)} />} />)}
         </FormGroup>
+        <TextField label='search' onChange={e => setSearchText(e.target.value)}/>
         {glossaryItems.map(item => (
           <div key={item.glossaryItem}>
             <h2>{item.glossaryItem}</h2>
